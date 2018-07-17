09677d8fc8e35a4d6c4cd923873f75aee1b251ef
hacraven <hacraven1@gmail.com>
2018-07-17 19:48:40 -0400

    solution

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,13 @@
 // Challenge 1
-// first try
+// 
 
 
-let = coffee
-coffee = 4
+ const coffee = 4
 
 if (coffee < 3) {
-   console.log ("I'm okay for now")
+   console.log("I'm okay for now")
 } 
 
-else if (time < 3) {
-   console.log ("Yes I'll take another cup of coffee")
+else if (coffee > 3) {
+   console.log("Yes I'll take another cup of coffee")
  }
