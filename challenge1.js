1da1c8dc751e4b35acd8b8f035501745cbd568f4
hacraven <hacraven1@gmail.com>
2018-07-17 19:37:52 -0400

    challenge 1

---
+++ challenge1.js
@@ -0,0 +1,14 @@
+// Challenge 1
+// first try
+
+
+let = coffee
+coffee = 4
+
+if (coffee < 3) {
+   console.log ("I'm okay for now")
+} 
+
+else if (time < 3) {
+   console.log ("Yes I'll take another cup of coffee")
+ }
